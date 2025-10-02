4bd95da555a558cc0292455575bfdbde5d013030
JumyazovMahmud <mahmudjumyazov919@gmail.com>
2025-10-02 12:36:57 +0500

    add SplashScreen text

--- app/src/main/java/uz/itteacher/mygithubapp/SplashScreen.kt
+++ app/src/main/java/uz/itteacher/mygithubapp/SplashScreen.kt
@@ -1,9 +1,11 @@
 package uz.itteacher.mygithubapp
 
 import android.window.SplashScreen
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SplashScreen(){
+    Text(text = "Splash Screen")
 
 }
